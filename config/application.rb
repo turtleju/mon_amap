15beb7b8c549fc02ae2cab64c4175a9279591662
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-09-21 14:19:11 +0200

    config generator

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,13 @@ module MonAmap
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |generator|
+      generator.helper false
+      generator.stylesheets false
+      generator.javascripts false
+      generator.view_specs false
+      generator.system_tests nil
+    end
   end
 end
